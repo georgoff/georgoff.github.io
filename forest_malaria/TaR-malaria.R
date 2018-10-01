ee5fbf68ce1ae2e59b8454178aaefcd297944f09
Alec Georgoff <georgoff@uw.edu>
2018-10-01 16:56:40 -0700

    deleted a bunch of old stuff from TaR-malaria.R

--- forest_malaria/TaR-malaria.R
+++ forest_malaria/TaR-malaria.R
@@ -40,19 +40,11 @@ r <- 1/200 # rate that humans recover from an infection
 n <- 12 # time for sporogonic cycle
 S <- a/g # stability index
 
-p <- 0.3
-
 ###################################
 #
 # Establish matrices of variables
 #
 ###################################
-
-## TO-DO: ask for number of villages/forests ##
-# readinteger <- function() {
-#   n <- readline(prompt = "Enter an integer: ")
-#   return(as.integer(n))
-# }
 
 # read in village and forest parameters from .csv file:
 
@@ -63,44 +55,12 @@ n_villages <- nrow(params)
 H <- params$H
 X <- vector(mode = "numeric", length = length(H))
 
-# village_params <- as.data.table(read.csv(village_params_path))
-# forest_params <- as.data.table(read.csv(forest_params_path))
-# all_params <- rbindlist(list(village_params, forest_params))
-# 
-# H_v <- village_params$H
-# V_v <- village_params$V
-# X_v <- village_params$X
-# Y_v <- village_params$Y
-# 
-# H_f <- forest_params$H
-# V_f <- forest_params$V
-# X_f <- forest_params$X
-# Y_f <- forest_params$Y
-# 
-# H <- c(H_v, H_f)
-# V <- c(V_v, V_f)
-# X <- c(X_v, X_f)
-# Y <- c(Y_v, Y_f)
-# 
-# n_villages <- nrow(village_params)
-# n_forests <- nrow(forest_params)
-# n_total <- n_villages + n_forests
-
-# # set up function to calculate R values:
-# calculate_R <- function(V, a, b, c, g, n, H, r) {
-#   R = (V * a^2 * b * c * exp(-g * n)) / (H * g * r)
-#   return(R)
-# }
-# 
-# # calculate R values:
-# R <- calculate_R(V, a, b, c, g, n, H, r)
-
-# Psi <- matrix(data = 0, nrow = n_total, ncol = n_total)
-# Psi <- matrix(c(1,1-p,0,p), nrow=2)
 Psi <- as.data.table(read.csv(psi_path))
 Psi[, id := NULL]
 Psi <- as.matrix(Psi)
 Psi_dt <- as.data.table(Psi)
+
+locs <- names(Psi_dt)
 
 H_psi <- t(Psi) %*% H
 X_psi <- t(Psi) %*% X
@@ -108,7 +68,6 @@ X_psi <- t(Psi) %*% X
 # choose starting point for root solver:
 theta_start <- vector(mode = "numeric", length = length(H))
 theta_start[1:length(theta_start)] <- 0.9
-# theta_start <- c(0.9, 0.9)
 
 # convert to number of humans:
 X_start <- theta_start * H
@@ -152,19 +111,8 @@ find_roots <- function(R,
                   S_val = S.,
                   H = H.)
   
-  # convert results to prevalence:
-  # theta_SS <- ss$root / (t(Psi) %*% H)
-  
-  X_psi_SS <- t(Psi) %*% ss$root
-  theta_SS <- X_psi_SS / H_psi
-  # theta_v_SS <- ss$root[1] / H_v
-  # theta_f_SS <- ss$root[2] / H_f
-  
   return(ss)
 }
-
-
-# create table of all possible R values and cycle thru each row of that
 
 R_min <- 0
 R_max <- 2
@@ -172,32 +120,37 @@ R_step <- 0.5
 
 all_R_values <- seq(R_min, R_max, R_step)
 
-results <- as.data.table(expand.grid(v1 = all_R_values,
-                                     v2 = all_R_values,
-                                     v3 = all_R_values,
-                                     f1 = all_R_values,
-                                     f2 = all_R_values))
+list_of_R_values <- list(NULL)
 
-results[, theta_v1 := 0]
-results[, theta_v2 := 0]
-results[, theta_v3 := 0]
-results[, theta_f1 := 0]
-results[, theta_f2 := 0]
+for (i in 1:length(locs)) {
+  list_of_R_values[[i]] <- all_R_values
+  
+  names(list_of_R_values)[i] <- locs[i]
+}
+
+results <- as.data.table(expand.grid(list_of_R_values))
+
+
+theta_holder <- as.data.table(matrix(data = 0, nrow = nrow(results), ncol = length(locs)))
+
+for (k in 1:length(locs)) {
+  names(theta_holder)[k] <- paste0("theta_", locs[k])
+}
+
+results <- cbind(results, theta_holder)
 
 for (i in 1:nrow(results)) {
   cat("Working on ", i, " of ", nrow(results), "\n")
   
-  these_R_values <- unlist(results[i, 1:5], use.names = FALSE)
+  these_R_values <- unlist(results[i, 1:length(locs)], use.names = FALSE)
   
   X_solutions <- find_roots(these_R_values)$root
   
   theta_solutions <- (t(Psi) %*% X_solutions) / H_psi
   
-  results[i, 6:10 := list(theta_solutions[1],
-                          theta_solutions[2],
-                          theta_solutions[3],
-                          theta_solutions[4],
-                          theta_solutions[5])]
+  for (j in (1 + length(locs)):ncol(results)) {
+    results[i, j] <- theta_solutions[j - length(locs)]
+  }
 }
 
 
@@ -226,54 +179,6 @@ for (j in 1:500) {
 }
 
 dev.off()
-
-# results2 <- as.data.table(expand.grid(names(Psi_dt) = all_R_values))
-
-# create data table to store results:
-# results <- data.table(R_0_v = rep(0, times = length(R_0_f_values) * length(R_0_v_values)), R_0_f = 0,
-#                       theta_v = 0, theta_f = 0,
-#                       X_v = 0, X_f = 0,
-#                       X_psi_v = 0, X_psi_f = 0,
-#                       root_f_value_v = 0, root_f_value_f = 0,
-#                       iter = 0, estim.precis = 0)
-
-i <- 1
-
-for (v in R_0_v_values) {
-  for (f in R_0_f_values) {
-    # record current R values:
-    results[i, R_0_v := as.numeric(v)]
-    results[i, R_0_f := as.numeric(f)]
-    # solve for roots at those R values:
-    these_roots <- find_roots(R = c(v,f))
-    
-    # add X values to results:
-    results[i, X_v := these_roots$root[1]]
-    results[i, X_f := these_roots$root[2]]
-    
-    # add scaled X values to results:
-    results[i, X_psi_v := X_v + (1-p)*X_f]
-    results[i, X_psi_f := p*X_f]
-    
-    # add prevalence values to results:
-    results[i, theta_v := X_psi_v / H_psi[1]]
-    results[i, theta_f := X_f / H[2]]
-    
-    # add value of equations at root to results:
-    results[i, root_f_value_v := these_roots$f.root[1,]]
-    results[i, root_f_value_f := these_roots$f.root[2,]]
-    
-    # add # of iterations and estimated precision to results:
-    results[i, iter := these_roots$iter]
-    results[i, estim.precis := these_roots$estim.precis]
-    
-    # print progress:
-    cat("R_0_v =", v, ", R_0_f =", f, " \r", file = "", sep = " ")
-    flush.console()
-    
-    i <- i + 1
-  }
-}
 
 heatmap <- plot_ly(x = results$R_0_v,
              y = results$R_0_f,

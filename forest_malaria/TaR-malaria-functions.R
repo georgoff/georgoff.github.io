931dd8234647b00990c349beed1a4c940ee32347
Alec Georgoff <georgoff@uw.edu>
2018-10-10 13:46:45 -0700

    starting to function-ize solver script

---
+++ forest_malaria/TaR-malaria-functions.R
@@ -0,0 +1,207 @@
+########################
+# TaR Malaria
+#
+# Author: Alec Georgoff
+#
+# Purpose: Solve for equilibrium prevalence values given R values in a complex system
+########################
+
+require(rootSolve)
+require(data.table)
+require(plotly)
+require(ggplot2)
+
+###################################
+#
+# Set parameters
+#
+###################################
+
+# specify filepaths for parameter .csv files:
+
+params_path <- "/homes/georgoff/georgoff.github.io/forest_malaria/params.csv"
+psi_path <- "/homes/georgoff/georgoff.github.io/forest_malaria/psi.csv"
+
+# set R values to cycle over:
+
+R_min <- 0
+R_max <- 2
+R_step <- 0.5
+
+# PDF output settings:
+output_PDF <- TRUE
+pdf_filepath <- "/homes/georgoff/georgoff.github.io/forest_malaria/test2.pdf"
+
+# this script assumes that the following parameters are the same for every
+# location; in reality this may not be accurate. an update may be made that
+# allows for custom parameters in every location
+
+a <- 0.88   # human blood feeding rate
+b <- 0.55   # proportion of bites by infectious mosquitoes that cause an infection
+c <- 0.15   # proportion of mosquitoes infected after biting infectious human
+g <- 0.1    # per capita death rate of mosquitoes
+r <- 1/200  # rate that humans recover from an infection
+n <- 12     # time for sporogonic cycle
+S <- a/g    # stability index
+
+###################################
+#
+# Establish matrices of variables
+#
+###################################
+
+# read in village and forest parameters from .csv file:
+
+params <- as.data.table(read.csv(params_path))
+
+n_villages <- nrow(params)
+
+H <- params$H
+X <- vector(mode = "numeric", length = length(H))
+
+Psi <- as.data.table(read.csv(psi_path))
+Psi[, id := NULL]
+Psi <- as.matrix(Psi)
+Psi_dt <- as.data.table(Psi)
+
+locs <- names(Psi_dt)
+
+H_psi <- t(Psi) %*% H
+X_psi <- t(Psi) %*% X
+
+# choose starting point for root solver:
+theta_start <- vector(mode = "numeric", length = length(H))
+theta_start[1:length(theta_start)] <- 0.9
+
+# convert to number of humans:
+X_start <- theta_start * H
+
+###################################
+#
+# Set up the equations as a function
+#
+###################################
+
+model <- function(X, Psi, R, c_val, S_val, H) {
+
+  theta_psi <- (t(Psi) %*% X) / (t(Psi) %*% H)
+
+  equation_vector <- (Psi %*% (R * (theta_psi/(c_val*S_val*theta_psi + 1)))) *
+    (H-X) - X
+
+  return(equation_vector)
+
+}
+
+###################################
+#
+# Solve for roots
+#
+###################################
+
+find_roots <- function(R,
+                       Psi. = Psi,
+                       H. = H,
+                       S. = S,
+                       c_val = c,
+                       X_start. = X_start) {
+
+  # use multiroot solver to find roots:
+  ss <- multiroot(f = model, start = X_start.,
+                  positive = TRUE, maxiter = 1000,
+                  ctol = 1e-20,
+                  Psi = Psi.,
+                  R = R,
+                  c_val = c_val,
+                  S_val = S.,
+                  H = H.)
+
+  return(ss)
+}
+
+###################################
+#
+# Set up results table
+#
+###################################
+create_results_table <- function()
+
+all_R_values <- seq(R_min, R_max, R_step)
+
+list_of_R_values <- list(NULL)
+
+for (i in 1:length(locs)) {
+  list_of_R_values[[i]] <- all_R_values
+
+  names(list_of_R_values)[i] <- locs[i]
+}
+
+# fill results table with every possible combination of
+# R values:
+
+results <- as.data.table(expand.grid(list_of_R_values))
+
+# put in placeholder for theta values:
+
+theta_holder <- as.data.table(matrix(data = 0, nrow = nrow(results),
+                                     ncol = length(locs)))
+
+for (k in 1:length(locs)) {
+  names(theta_holder)[k] <- paste0("theta_", locs[k])
+}
+
+results <- cbind(results, theta_holder)
+
+###################################
+#
+# Cycle through R values
+#
+###################################
+
+for (i in 1:nrow(results)) {
+  cat("Working on ", i, " of ", nrow(results), "\n")
+
+  these_R_values <- unlist(results[i, 1:length(locs)], use.names = FALSE)
+
+  X_solutions <- find_roots(these_R_values)$root
+
+  theta_solutions <- (t(Psi) %*% X_solutions) / H_psi
+
+  for (j in (1 + length(locs)):ncol(results)) {
+    results[i, j] <- theta_solutions[j - length(locs)]
+  }
+}
+
+###################################
+#
+# Create PDF of results
+#
+###################################
+
+if (output_PDF) {
+  pdf(pdf_filepath)
+
+  this_row_locs <- vector(mode = "character", length = length(locs))
+
+  for (j in 1:500) {
+    for (location in 1:length(locs)) {
+      this_row_locs[location] <- paste0(locs[location],
+                                        "\nR = ",
+                                        as.character(results[j, ..location]))
+    }
+
+    this_row <- data.table(loc = this_row_locs,
+                           theta = unlist(results[j, (1+length(locs)):ncol(results)],
+                                          use.names = F))
+
+    bar <- ggplot(data = this_row,
+                  aes(x = loc, y = theta)) +
+      geom_col() +
+      geom_text(aes(label = round(theta, 3), y = theta + 0.02)) +
+      coord_cartesian(ylim = c(0,0.31))
+
+    print(bar)
+  }
+
+  dev.off()
+}
--- forest_malaria/forest-app/app.R
+++ forest_malaria/forest-app/app.R
@@ -3,60 +3,133 @@ library(data.table)
 
 # source("/georgoff.github.io/forest_malaria/TaR-malaria.R")
 
+# ui <- fluidPage(
+#   titlePanel("Forest Malaria Simulator"),
+#   
+#   sidebarLayout(
+#     
+#     sidebarPanel(
+#       numericInput(inputId = "a",
+#                    label = "a: Human Blood Feeding Rate",
+#                    value = 0.88,
+#                    min = 0, max = 1, step = 0.01),
+#       numericInput(inputId = "b",
+#                    label = "b: Proportion of Bites by Infectious Mosquitoes That Cause an Infection",
+#                    value = 0.55,
+#                    min = 0, max = 1, step = 0.01),
+#       numericInput(inputId = "c",
+#                    label = "c: Proportion of Mosquitoes Infected After Biting Infectious Human",
+#                    value = 0.15,
+#                    min = 0, max = 1, step = 0.01),
+#       numericInput(inputId = "g",
+#                    label = "g: Per Capita Death Rate of Mosquitoes",
+#                    value = 0.1,
+#                    min = 0, max = 1, step = 0.01),
+#       numericInput(inputId = "n",
+#                    label = "n: Time for Sporogonic Cycle",
+#                    value = 12,
+#                    min = 0, step = 0.5),
+#       numericInput(inputId = "r",
+#                    label = "r: Rate that Humans Recover from an Infection",
+#                    value = 1/200,
+#                    min = 0),
+#       
+#       fileInput(inputId = "params_csv",
+#                 label = "Upload params.csv",
+#                 accept = c("text/csv",
+#                            "text/comma-separated-values,text/plain",
+#                            ".csv")),
+#       fileInput(inputId = "psi_csv",
+#                 label = "Upload psi.csv",
+#                 accept = c("text/csv",
+#                            "text/comma-separated-values,text/plain",
+#                            ".csv"))
+#       
+#       # submitButton(text = "GO")
+#     ),
+#     
+#     mainPanel(
+#       tabsetPanel(
+#         tabPanel("Constants",
+#                  tableOutput(outputId = "constants_table")),
+#         tabPanel("Parameters",
+#                  tableOutput(outputId = "params_table"),
+#                  tableOutput(outputId = "psi_table"))
+#       )
+#     )
+#   )
+#   
+# )
+
 ui <- fluidPage(
   titlePanel("Forest Malaria Simulator"),
   
-  sidebarLayout(
+  tabsetPanel(
+    tabPanel("Constants",
+             sidebarLayout(
+               sidebarPanel(numericInput(inputId = "a",
+                                         label = "a: Human Blood Feeding Rate",
+                                         value = 0.88,
+                                         min = 0, max = 1, step = 0.01),
+                            numericInput(inputId = "b",
+                                         label = "b: Proportion of Bites by Infectious Mosquitoes That Cause an Infection",
+                                         value = 0.55,
+                                         min = 0, max = 1, step = 0.01),
+                            numericInput(inputId = "c",
+                                         label = "c: Proportion of Mosquitoes Infected After Biting Infectious Human",
+                                         value = 0.15,
+                                         min = 0, max = 1, step = 0.01),
+                            numericInput(inputId = "g",
+                                         label = "g: Per Capita Death Rate of Mosquitoes",
+                                         value = 0.1,
+                                         min = 0, max = 1, step = 0.01),
+                            numericInput(inputId = "n",
+                                         label = "n: Time for Sporogonic Cycle",
+                                         value = 12,
+                                         min = 0, step = 0.5),
+                            numericInput(inputId = "r",
+                                         label = "r: Rate that Humans Recover from an Infection",
+                                         value = 1/200,
+                                         min = 0)),
+               
+               mainPanel(tableOutput(outputId = "constants_table")))
+             ),
     
-    sidebarPanel(
-      numericInput(inputId = "a",
-                   label = "a: Human Blood Feeding Rate",
-                   value = 0.88,
-                   min = 0, max = 1, step = 0.01),
-      numericInput(inputId = "b",
-                   label = "b: Proportion of Bites by Infectious Mosquitoes That Cause an Infection",
-                   value = 0.55,
-                   min = 0, max = 1, step = 0.01),
-      numericInput(inputId = "c",
-                   label = "c: Proportion of Mosquitoes Infected After Biting Infectious Human",
-                   value = 0.15,
-                   min = 0, max = 1, step = 0.01),
-      numericInput(inputId = "g",
-                   label = "g: Per Capita Death Rate of Mosquitoes",
-                   value = 0.1,
-                   min = 0, max = 1, step = 0.01),
-      numericInput(inputId = "n",
-                   label = "n: Time for Sporogonic Cycle",
-                   value = 12,
-                   min = 0, step = 0.5),
-      numericInput(inputId = "r",
-                   label = "r: Rate that Humans Recover from an Infection",
-                   value = 1/200,
-                   min = 0),
-      
-      fileInput(inputId = "params_csv",
-                label = "Upload params.csv",
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      fileInput(inputId = "psi_csv",
-                label = "Upload psi.csv",
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv"))
-      
-      # submitButton(text = "GO")
-    ),
+    tabPanel("Parameters",
+             sidebarLayout(
+               sidebarPanel(fileInput(inputId = "params_csv",
+                                      label = "Upload params.csv",
+                                      accept = c("text/csv",
+                                                 "text/comma-separated-values,text/plain",
+                                                 ".csv")),
+                            fileInput(inputId = "psi_csv",
+                                      label = "Upload psi.csv",
+                                      accept = c("text/csv",
+                                                 "text/comma-separated-values,text/plain",
+                                                 ".csv"))),
+               
+               mainPanel(tableOutput(outputId = "params_table"),
+                         tableOutput(outputId = "psi_table"))
+             )
+             ),
     
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Constants",
-                 tableOutput(outputId = "constants_table")),
-        tabPanel("Parameters",
-                 tableOutput(outputId = "params_table"),
-                 tableOutput(outputId = "psi_table"))
-      )
-    )
+    tabPanel("R Values",
+             sidebarLayout(
+               sidebarPanel(numericInput(inputId = "R_min",
+                                         label = "Minimum R Value",
+                                         value = 0,
+                                         min = 0, step = 0.5),
+                            numericInput(inputId = "R_max",
+                                         label = "Maximum R Value",
+                                         value = 3,
+                                         min = 0, step = 0.5),
+                            numericInput(inputId = "R_step",
+                                         label = "Step Size Between R Values",
+                                         value = 0.1,
+                                         min = 0, step = 0.1)),
+               
+               mainPanel(textOutput(outputId = "R_statement"))
+             ))
   )
   
 )
@@ -66,8 +139,8 @@ server <- function(input, output) {
     table <- as.data.table(matrix(nrow = 6, ncol = 2))
     names(table) <- c("Constant", "Value")
     table[, "Constant"] <- c("a", "b", "c", "g", "n", "r")
-    table[, "Value"] <- c(input$a, input$b, input$c,
-                          input$g, input$n, input$r)
+    table[, "Value"] <- as.character(c(input$a, input$b, input$c,
+                          input$g, input$n, input$r))
     
     table
   })
@@ -90,6 +163,11 @@ server <- function(input, output) {
     }
     
     read.csv(inFile$datapath)
+  })
+  
+  output$R_statement <- renderText({
+    paste0("R will range from ", as.character(input$R_min), " to ", as.character(input$R_max),
+              ", with a step size of ", as.character(input$R_step))
   })
 }
 
